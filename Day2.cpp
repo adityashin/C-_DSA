ff4ec943d25e4d794d9ef2023c2ded844880c82a
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-09-10 23:51:06 +0530

    Add files via upload

---
+++ Day2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"aditya "<<"shinde"<<endl;
+    //Homework
+    int a=100;
+    int b=1000;
+    int sum=a+b;
+    int diff = a-b;
+
+    cout<<"sum of a and b is "<<sum<<'\n';
+
+    cout<<"Difference between two numbers are "<<diff <<'\n';
+
+    int product = 1;
+    int quantity = 100;
+    int totalnumberofproducts = product * quantity;
+    cout<<"Total number of products sold are "<<totalnumberofproducts<<endl;
+
+    //division
+    cout<<"division of a and b is"<<a/b<<endl;
+
+    //module
+    cout<<"Module of a and b is "<<a%b<<endl;
+
+
+    return 0;
+}
---
+++ Day3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int age ;
+
+    // cout<<"Check wheather number is even or odd\n";
+    // cout<<"Enter a number\n";
+    // cin>>age;
+
+    // if(age>=18){
+    //     cout<<"eligible to vote";
+    // }else{
+    //     cout<<"Not eligible to vote";
+    // }
+
+    //check number is divisible to 2 or not 
+    // int num;
+    // cout<<"Enter any number to check it is divisible by 2 or not\n";
+    // cout<<"Enter any number\n";
+    // cin>>num;
+
+    // if(num%2==0){
+    //     cout<<"it is divisible to 2\n";
+    // }else{
+    //     cout<<"IT is not divisible to 2\n";
+    // }
+
+
+    //check lowercase or uppercase
+    char ch;
+    cout<<"check any lowercase or uppercase character\n\n";
+
+    cout<<"Enter any character\n";
+    cin>>ch;
+    if(ch >=65 && ch <=90){
+        cout<<"IT is uppercase";
+    }else{
+        cout<<"IT is lowercase";
+    }
+
+
+
+    return 0;
+
+
+}
