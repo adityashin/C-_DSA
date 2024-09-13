d7bb0f05b7e976a0cd765930a07d12a54db7d5b1
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-09-13 19:39:46 +0530

    Update Day4.cpp
    little bit of code added

--- Day4.cpp
+++ Day4.cpp
@@ -1,24 +1,82 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n =4;
+int main()
+{
+    int n = 4;
 
-    for(int i=0;i<=n;i++){  //outer loop
-        for(int j=0;j<=n;j++){  //inner loop
-            cout<<"*"<<" ";
+    for (int i = 0; i <= n; i++)
+    { // outer loop
+        for (int j = 0; j <= n; j++)
+        { // inner loop
+            cout << "*" << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
-
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         char ch = 'A';
-        for(int j=0;j<n;j++){
-            cout<<ch<<" ";
+        for (int j = 0; j < n; j++)
+        {
+            cout << ch << " ";
             ch = ch + 1;
         }
-        cout<<endl;
+        cout << endl;
+    }
+
+    {
+        int n = 3;
+        int no = 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << no << " ";
+                no = no + 1;
+            }
+            cout << endl;
+        }
+    }
+
+    { // Homework
+        // print a to i
+        char ch = 'A';
+        int no = 3;
+        for (int i = 0; i < no; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+
+                cout << ch << " ";
+                ch = ch + 1;
+            }
+
+            cout << endl;
+        }
+    }
+
+    // Print Right Triangle
+    {
+        int no = 50;
+        for (int i = 0; i <= no; i++)
+        { // outer
+            for (int j = 0; j < i + 1; j++)
+            { // inner
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+
+    //print 5 lines with same number
+    {
+        int no=5;
+        for(int i=1;i<=no;i++){
+            for(int j=1;j<=i;i++){
+                
+            }
+        }
     }
     return 0;
 }
