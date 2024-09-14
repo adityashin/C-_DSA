bf0d542185b655cdbd03e4a09ceb0063ce2a3d69
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-09-14 23:24:33 +0530

    Update Day4.cpp
    New Patterns added

--- Day4.cpp
+++ Day4.cpp
@@ -58,7 +58,7 @@ int main()
 
     // Print Right Triangle
     {
-        int no = 50;
+        int no = 5;
         for (int i = 0; i <= no; i++)
         { // outer
             for (int j = 0; j < i + 1; j++)
@@ -69,14 +69,89 @@ int main()
         }
     }
 
-    //print 5 lines with same number
-    {
-        int no=5;
-        for(int i=1;i<=no;i++){
-            for(int j=1;j<=i;i++){
-                
+   //print from 1 in square pattern
+   {
+    int no=1;
+    int looprun=3;
+    for(int i=0;i< looprun;i++){
+        for(int j=0;j< looprun;j++){
+            cout<<no<<" ";
+            no++;
+        }
+        cout<<endl;
+    }
+   }
+
+   //print from A in square pattern
+   {
+    char ch='A';
+    int loopnum = 3;
+    for(int i=0;i<loopnum;i++){
+        for(int j=0;j<loopnum;j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+   }
+
+   //triangle pattern
+   {
+    int loopnum = 5;
+    for(int i=0;i<loopnum;i++){
+        for(int j=0;j<i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+   }
+
+   //triangle pattern 
+   {
+    int loopnum=5;
+    for(int i=0;i<loopnum;i++){
+        int no =1;
+        for(int j=0;j<i+1;j++){
+            cout<<no;  
+            no++;
+        }
+        cout<<endl;
             }
+   }
+
+
+
+   // Decrement operator starts here
+   // from here we will execute the programs of patterns using decreement 
+   {
+    int n=7;
+    for(int i=n;i>=0;i--){
+        cout<<i<<endl;
+    }
+   }
+
+    //print reverse triangle 
+   {
+    int no=4;
+    for(int i=0;i<=no;i++){
+        for(int j=i+1;j>=0;j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    //Floyd's Triangle patterm
+    {
+        int no=1;
+        int loopnum = 5;
+        for(int i=1;i<=loopnum;i++){
+            for(int j=1;j<=i;j++){
+                cout<<no<<" ";
+                no++;
+            }
+            cout<<endl;
         }
     }
+   }
     return 0;
 }
